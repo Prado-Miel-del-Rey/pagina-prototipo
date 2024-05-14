027bfb030eed67e978dc95d3e01bfdd9e32672ce
Paddy <ohanlon.paddy@gmail.com>
2024-05-14 10:01:11 +0200

    Add coloured stick to footer logo Update legal links Refine SVG icon

--- js/app.js
+++ js/app.js
@@ -18,7 +18,15 @@
     throw new Error("Primary nav elements are missing from the DOM");
   }
 
+  let isToggling = false;
+
   function togglePrimaryMenu(event) {
+    if (isToggling) return;
+    isToggling = true;
+    setTimeout(() => {
+      isToggling = false;
+    }, 300); // Adjust timeout as needed
+
     event.stopPropagation();
 
     const isClosing = primaryNav.classList.contains("is-open");
@@ -32,27 +40,30 @@
     primaryNav.setAttribute("aria-expanded", !isClosing);
     primaryNavToggle.setAttribute("aria-expanded", !isClosing);
 
-    // Close with ECS key
-    function toggleOnEsc(event) {
-      if (event.key === "Escape" || event.key === "Esc") {
-        togglePrimaryMenu(event);
-      }
-    }
-    function closeOnClickOutside(event) {
-      if (event.target === primaryNavBackdrop) {
-        togglePrimaryMenu(event);
-      }
-    }
+    // Add or remove event listeners based on the state of the menu
     if (isClosing) {
+      document.removeEventListener("keydown", toggleOnEsc);
       document.removeEventListener("click", closeOnClickOutside);
       primaryNavToggle.focus();
     } else {
-      document.addEventListener("keydown", toggleOnEsc, { once: true });
+      document.addEventListener("keydown", toggleOnEsc);
       document.addEventListener("click", closeOnClickOutside);
       const firstLink = primaryNav.querySelector("a");
       if (firstLink) {
         firstLink.focus();
       }
+    }
+  }
+
+  function toggleOnEsc(event) {
+    if (event.key === "Escape" || event.key === "Esc") {
+      togglePrimaryMenu(event);
+    }
+  }
+
+  function closeOnClickOutside(event) {
+    if (event.target === primaryNavBackdrop) {
+      togglePrimaryMenu(event);
     }
   }
 
